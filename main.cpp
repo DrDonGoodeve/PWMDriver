bf031740566bd57e434dcd0ca803435da0bd3d58
Don Goodeve <don@radiocode.ca>
2022-11-30 08:16:31 -0800

    Cleaned up main.cpp

--- main.cpp
+++ main.cpp
@@ -36,125 +36,90 @@
 #define kClkFrequency       (125.0e6f)
 #define kPulseFrequency     (1.0f)
 #define kLEDUpdateFreq      (1000.0f)
-
-
-#define LED_PIN 16  // GPIO 16
-#define LED_LENGTH 1
-#define kPi (3.141592654f)
-
+#define kRGBLEDGPIO         (16)  // RP Zero allocation
+#define kPi                 (3.141592654f)
 #define _min(a,b)           (((a)<(b))?(a):(b))
 
-/*
-// Heartbeat - conventional PWMDriver implementation (PWMwrap-interrupt-driven)
-//-------------------------------------
-class Heartbeat : public PWMDriver::Source,
-                  public PWMDriver::Group {
-    
+
+//*** Local types
+//*****************************************************************************
+class Heartbeat {
     private:
-        bool mbCountingUp;
-        float mfValue;
-        float mfIncrement;
+        WS2812 mcLEDs;
+        float mfPhase;
+        float mfPhaseShift;
+        float mfAmplitude;
+        bool mbRising;
 
-    public:
-        Heartbeat(uint uGPIO) :
-            PWMDriver::Source(uGPIO),
-            mbCountingUp(true), mfValue(0.0f), mfIncrement(0.0f) {
+        struct repeating_timer mcHeartbeatTimer;
 
-            addSource(this);
-            PWMDriver::instance()->addGroup(this);
+        static Heartbeat *mspSelf;
+
+        static bool timerCallback(struct repeating_timer *pTimer) {
+            if (mspSelf != nullptr) {
+                return mspSelf->heartbeatCallback();
+            }
+            return false;
         }
 
-        ~Heartbeat(void) {
-            PWMDriver::instance()->removeGroup(this);
-            removeSource(this);
-        }
+        bool heartbeatCallback(void) {
+            float fRed(sinf(mfPhase)), fGreen(sinf(mfPhase+mfPhaseShift)), fBlue(sinf(mfPhase+(2.0f*mfPhaseShift)));
+            fRed = (fRed*fRed)*mfAmplitude; 
+            fGreen = (fGreen*fGreen)*mfAmplitude; 
+            fBlue = (fBlue*fBlue)*mfAmplitude; 
+            uint8_t uRed((uint8_t)roundf(fRed*255.0f));
+            uint8_t uGreen((uint8_t)roundf(fGreen*255.0f));
+            uint8_t uBlue((uint8_t)roundf(fBlue*255.0f));
 
-        // Subclass required implementations
-        virtual float getDesiredUpdateFrequency(void) {
-            return kLEDUpdateFreq;
-        }
+            mcLEDs.fill(WS2812::RGB(uRed, uGreen, uBlue));
+            mcLEDs.show();
 
-        virtual void resetSequence(void) {
-            mbCountingUp = true;
-            mfValue = 0.0f;
-            mfIncrement = (2.0f * kPulseFrequency) / mfSampleRateHz;
-        }
-
-        virtual float getNextSequence(void) {
-            //printf("Heartbeat::getNextSequence - %.2f\r\n", mfValue);
-            if (true == mbCountingUp) {
-                mfValue += mfIncrement;
-                if (mfValue >= 1.0f) {
-                    mfValue = 1.0f;
-                    mbCountingUp = false;
+            if (true == mbRising) {
+                mfAmplitude += 2.0e-2f;
+                if (mfAmplitude >= 1.0f) {
+                    mbRising = false;
                 }
             } else {
-                mfValue -= mfIncrement;
-                if (mfValue <= 0.0f) {
-                    mfValue = 0.0f;
-                    mbCountingUp = true;
+                mfAmplitude -= 2.0e-2f;
+                if (mfAmplitude <= 0.0f) {
+                    mbRising = true;
+                    mfPhase = mfPhase+(kPi/26.7f);
+                    mfPhase = (mfPhase>(2.0f*kPi))?(mfPhase-(2.0f*kPi)):mfPhase;
+                    mfPhaseShift += (kPi/7.0f);
+                    mfPhaseShift = (mfPhaseShift>(2.0f*kPi))?(mfPhaseShift-(2.0f*kPi)):mfPhaseShift;
                 }
             }
-            return mfValue;
+            mfPhase = (mfPhase>(2.0f*kPi))?(mfPhase-(2.0f*kPi)):mfPhase;
+
+            return true;
+        }
+
+    public:
+        Heartbeat(void) :
+            mcLEDs(kRGBLEDGPIO, 1, pio0, 0, WS2812::FORMAT_GRB),
+            mfPhase(0.0f), mfAmplitude(0.1f), mbRising(true), mfPhaseShift(kPi/1.3f) {
+            mspSelf = this;
+
+            // Startup colourful heartbeat
+            add_repeating_timer_ms(10, timerCallback, NULL, &mcHeartbeatTimer);
+        }
+
+        ~Heartbeat() {
+            cancel_repeating_timer(&mcHeartbeatTimer);
+            mspSelf = nullptr;
         }
 };
-*/
 
-static WS2812 *spLEDs = nullptr;
-static bool timerCallback(struct repeating_timer *pTimer){
-    // Ascending spiral RGB
-    static float fPhase(0.0f), fAmplitude(0.1f);
-    static float fPhaseShift(kPi/1.3f);
-    static bool bRising(true);
+Heartbeat *Heartbeat::mspSelf = nullptr;
 
-    float fRed(sinf(fPhase)), fGreen(sinf(fPhase+fPhaseShift)), fBlue(sinf(fPhase+(2.0f*fPhaseShift)));
-    fRed = (fRed*fRed)*fAmplitude; 
-    fGreen = (fGreen*fGreen)*fAmplitude; 
-    fBlue = (fBlue*fBlue)*fAmplitude; 
-    uint8_t uRed((uint8_t)roundf(fRed*255.0f));
-    uint8_t uGreen((uint8_t)roundf(fGreen*255.0f));
-    uint8_t uBlue((uint8_t)roundf(fBlue*255.0f));
 
-    spLEDs->fill(WS2812::RGB(uRed, uGreen, uBlue));
-    spLEDs->show();
-
-    if (true == bRising) {
-        fAmplitude += 1.0e-2f;
-        if (fAmplitude >= 1.0f) {
-            bRising = false;
-        }
-    } else {
-        fAmplitude -= 1.0e-2f;
-        if (fAmplitude <= 0.0f) {
-            bRising = true;
-            fPhase = fPhase+(kPi/26.7f);
-            fPhase = (fPhase>(2.0f*kPi))?(fPhase-(2.0f*kPi)):fPhase;
-        }
-    }
-
-    return true;
-}
 
 int main(void) {
     // Initialize stdio functionality
     stdio_init_all();
 
-    // 0. Initialize LED strip
-    WS2812 cLEDStrip(
-        LED_PIN,            // Data line is connected to pin 0. (GP0)
-        LED_LENGTH,         // Strip is 6 LEDs long.
-        pio0,               // Use PIO 0 for creating the state machine.
-        0,                  // Index of the state machine that will be created for controlling the LED strip
-                            // You can have 4 state machines per PIO-Block up to 8 overall.
-                            // See Chapter 3 in: https://datasheets.raspberrypi.org/rp2040/rp2040-datasheet.pdf
-        WS2812::FORMAT_GRB  // Pixel format used by the LED strip
-    );
-    spLEDs = &cLEDStrip;
+    Heartbeat cHeartbeat;
 
-    // Startup colourful heartbeat
-    struct repeating_timer cHeartbeatTimer;
-    add_repeating_timer_ms(10, timerCallback, NULL, &cHeartbeatTimer);
-  
     while(true) {
         tight_loop_contents();
     }
